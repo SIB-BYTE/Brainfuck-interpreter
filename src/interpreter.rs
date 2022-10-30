33166603bad97f580287031bc312ba59c64d009d
SIB-BYTE <97762248+SIB-BYTE@users.noreply.github.com>
2022-10-30 22:28:34 +0000

    Update interpreter.rs

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -13,7 +13,7 @@ pub fn map_file(file: String) -> String {
     }
 }
 
-// Preprocessing function that generates & returns a hashmap of positions
+// Preprocessing function that generates & returns a hashmap of positions:
 pub fn construct_jump_table(instruction_vector: &Vec<char>) -> HashMap<usize, usize> {
     let mut table_stack:  Vec<usize> = Vec::new(); // Used for constructing the table
     let mut balance_stack: Vec<char> = Vec::new(); // Used for balancing of '[' && ']'
@@ -22,19 +22,19 @@ pub fn construct_jump_table(instruction_vector: &Vec<char>) -> HashMap<usize, us
     for (pos, curr_char) in instruction_vector.iter().enumerate() {
         match curr_char {
             '[' => {
-                // balancing stuff:
+                // Balancing stack:
                 balance_stack.push(*curr_char);
 
-                // jump table stuff
+                // Jump table code:
                 table_stack.push(pos);
             },
 
             ']' => {
-                // balancing stuff:
+                // Balancing stack code:
                 if balance_stack.last() == None { panic!("Imbalanced brackets!"); }
                 balance_stack.pop();
 
-                // jump table stuff
+                // Jump table code:
                 let top_value = table_stack.last().cloned().unwrap();
                 table_stack.pop();
                 table.insert(top_value, pos);
@@ -53,13 +53,22 @@ pub fn construct_jump_table(instruction_vector: &Vec<char>) -> HashMap<usize, us
 pub fn execute(instruction_list: String) {
     if instruction_list.len() == 0 { panic!("Empty file"); }
 
+    // Vector representation of the mapped file contents:
     let instruction_vector: Vec<char> = instruction_list.chars().collect();
 
+    // Program tape:
     let mut program: [u8 ; 30_000]= [b'\0'; 30_000];
+    
+    // Bracket table construction for the instruction vector:
     let bracket_table: HashMap<usize, usize> = construct_jump_table(&instruction_vector);
 
+    // Program cell index:
     let mut cell_idx: usize = 0;
+    
+    // Program counter / current index into instruction_vector:
     let mut pc: usize = 0;
+    
+    // Interpretation loop:
     while pc < instruction_vector.len() {
         match instruction_vector[pc] {
             '<' => {
@@ -77,11 +86,11 @@ pub fn execute(instruction_list: String) {
             '.' => { print!("{}", program[cell_idx] as char); }
             ',' => {
                 program[cell_idx] = io::stdin()
-                    .bytes()
-                    .next()
-                    .and_then(|res| res.ok())
-                    .map(|byte| byte as u8)
-                    .unwrap();
+                                        .bytes()
+                                        .next()
+                                        .and_then(|result| result.ok())
+                                        .map(|byte| byte as u8)
+                                        .unwrap();
             }
 
             '[' => { if program[cell_idx] == 0 { pc = bracket_table[&pc]; } }
